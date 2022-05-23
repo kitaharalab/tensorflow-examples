199b9f7409a59449c2cebf0e773cf6d286f07358
fujiij <fujii@kthrlab.jp>
2022-05-23 10:10:07 +0900

    Update TFTestClient.java

--- app/src/main/java/TFTestClient.java
+++ app/src/main/java/TFTestClient.java
@@ -85,38 +85,5 @@ public class TFTestClient {
         // print(model.graph().toGraphDef())
     }
 
-    public void createModel() {
-        TFloat32 x = TFloat32.tensorOf(NdArrays.ofFloats(Shape.of(1, 6, 2)));
-        //TFloat32 x = TFloat32.tensorOf(NdArrays.ofFloats(Shape.of(1, 2)));
-        x.setFloat((float) 170, 0, 0, 0);
-        x.setFloat((float) 60, 0, 0, 1);
-        x.setFloat((float) 165, 0, 1, 0);
-        x.setFloat((float) 55, 0, 1, 1);
-        x.setFloat((float) 158, 0, 2, 0);
-        x.setFloat((float) 50, 0, 2, 1);
-        x.setFloat((float) 130, 0, 3, 0);
-        x.setFloat((float) 25, 0, 3, 1);
-        x.setFloat((float) 120, 0, 4, 0);
-        x.setFloat((float) 20, 0, 4, 1);
-        x.setFloat((float) 110, 0, 5, 0);
-        x.setFloat((float) 18, 0, 5, 1);
-
-        TFloat32 y = TFloat32.tensorOf(NdArrays.ofFloats(Shape.of(1, 6)));
-        y.setFloat(1, 0, 0);
-        y.setFloat(1, 0, 1);
-        y.setFloat(1, 0, 2);
-        y.setFloat(0, 0, 3);
-        y.setFloat(0, 0, 4);
-        y.setFloat(0, 0, 5);
-
-    }
-// x = np.array([[170, 60], [165, 55], [158, 50], [130, 25], [120, 20], [110, 18]])
-// y = np.array([[1], [1], [1], [0], [0], [0]])
-
-// model = tf.keras.Sequential()
-// model.add(tf.keras.layers.Dense(1, input_dim=2, use_bias=True, activation="sigmoid"))
-// model.compile(optimizer="adam", loss="binary_crossentropy", metrics="binary_accuracy")
-// model.fit(x, y, epochs=1000)
-// print(model.summary())
 
 }
